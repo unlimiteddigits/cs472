a2e469e0de37e0cedfb1cf30ee5a685014c5d4c8
unlimiteddigits <ericmw02@students.ipfw.edu>
2019-03-03 15:50:45 -0500

    added hw3

---
+++ hw3/q1/q1.c
@@ -0,0 +1,16 @@
+do {
+waiting[i] = true;
+key = true;
+while (waiting[i] && key)
+key = test and set(&lock);
+waiting[i] = false;
+/* critical section */
+j = (i + 1) % n;
+while ((j != i) && !waiting[j])
+j = (j + 1) % n;
+if (j == i)
+lock = false;
+else
+waiting[j] = false;
+/* remainder section */
+} while (true);
