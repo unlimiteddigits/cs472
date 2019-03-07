c49e2b75df2589d0e1102ed64e8e77343f8b74c8
unlimiteddigits <you@example.com>
2019-03-06 22:26:12 -0500

    worked on q2

--- hw3/q2/q2.c
+++ hw3/q2/q2.c
@@ -10,141 +10,68 @@
 int compare_and_swap(int *value, int expected, int new_value);
 int test_and_set(int *target) ;
 
-int size=100;
-int pid, pid1;
-int waiting[100];
-int n,i,j;
-int lock=0;
-int key=0;
-	int shmid;
-	key_t skey;
-	int *shm;
-	char *s;
-char singledigit[10];
+// http://faculty.salina.k-state.edu/tim/ossg/IPC_sync/ts.html
+// http://enggedu.com/tamilnadu/university_questions/question_answer/be_nd_2007/5th_sem/cse/cs1252/part_b/12_b_1.html
+void signal (semaphore S);
+void wait(semaphore S);
 
+struct semaphore {
+    int value = 0;
+    boolean mutex = FALSE;
+    boolean hold = TRUE;
+    queue m, h;
+};
 
-int main(int argc, char *argv[])
-{
-	skey=9876;
-	shmid = shmget(skey, size, IPC_CREAT | 0666);
-	//ftruncate(key, size);
-	if (shmid < 0)
-	{
-		perror("shmget in parent process.");
-		exit(1);
-	}
+struct semaphore s;
+
+int main(int argc, char *argv[]){
 	
-	shm=shmat(shmid, NULL,0);
-	*shm=1;
-	printf("after null%i\n",*shm);
-	
-	//perror("test perror");
-
-	if (shm == (int* )-1)
-	{
-		perror("shmat in parent process.");
-		exit(1);
-	}
-	
-	printf("%i\n",*shm);
-	sleep(2);
-
-
-	pid = fork();
-	if (pid < 0) { /* error occurred */
-	  fprintf(stderr, "Fork Failed");
-	  return 1;
-	}
-	else if (pid == 0) { /* child process */
-		//shmid = shmget(skey, size,  0666);
-		//shm=shmat(shmid, NULL,0);
-		pid1 = getpid();
-		printf("child: pid = %d\n",pid); /* C */
-		printf("child: pid1 = %d\n",pid1); /* D */
-		sleep(1);
-		printf("proceed after 1st sleep.\n");
-		*shm = 0;
-		sleep(1);
-		printf("proceed after 2nd sleep.%i\n", *shm);
-		*shm = 0;
-		exit(1);
-	}
-	else { /* parent process */
-	  pid1 = getpid();
-	  printf("parent: pid = %d\n",pid); /* C */
-	  printf("parent: pid1 = %d\n",pid1); /* D */
-//	  wait(NULL);
-	}
-	
-	i=0;
-	lock=1;
-	do {
-		while (test_and_set(&lock)!=0)
-		{
-			if (*shm == 0) {
-				lock=0;
-			}
-		}
-		; /* do nothing */
-		printf("in first do.  %i\n",i);
-		/* critical section */
-		lock = 0;
-		i++;
-		/* remainder section */
-	} while (i<10);	
-	
-	i=0;
-	lock=1;
-	*shm=1;
-	do {
-		while (compare_and_swap(&lock, 0, 1) != 0)
-			lock=*shm; /* do nothing */
-		printf("in second do.  %i\n",i);
-		/* critical section */
-		lock = 0;
-		i++;
-		/* remainder section */
-	} while (i<10);
-/*
-	do {
-		waiting[i] = 1;
-		key = 1;
-		while (waiting[i]==1 && key==1)
-			key = compare_and_swap(&lock,0,1);
-		waiting[i] = 0;
-		// * critical section * /
-		j = (i + 1) % n;
-		while ((j != i) && !waiting[j])
-			j = (j + 1) % n;
-		if (j == i)
-			lock = 0;
-		else
-			waiting[j] = 0;
-		/ * remainder section * /
-	} while (1);
-*//*
-	do {
-		waiting[i] = 1;
-		key = 1;
-		while (waiting[i]==1 && key==1)
-			key = test_and_set(&lock);
-		waiting[i] = 0;
-		// * critical section * /
-		j = (i + 1) % n;
-		while ((j != i) && !waiting[j])
-			j = (j + 1) % n;
-		if (j == i)
-			lock = 0;
-		else
-			waiting[j] = 0;
-		/ * remainder section * /
-	} while (1);
-*/
- if ( shmctl( shmid, IPC_RMID, NULL ) == -1 )
-         perror("failed to destroy");
-
 }
 
+void wait(semaphore S){
+	S.value--;
+	if(S.value<0){
+		add this process to S.L;
+		block();
+	}
+}
+
+void signal (semaphore S){
+	S.value++;
+	if(S.value <= 0){
+		remove a process P from S.L;
+		wakeup(P);
+	}
+}
+
+/* P() */
+/*
+acquire_semaphore(struct semaphore s) {
+    while(TS(s.mutex)) WAIT(s.m);    // wait for intertal mutex 
+    s.value--;
+    if(s.value < 0) {
+        s.mutex = FALSE;
+        SIGNAL(s.m);
+        while(TS(s.hold)) WAIT(s.h); // wait - too many out 
+    }
+    else
+        s.mutex = FALSE;
+        SIGNAL(s.m);
+}
+
+/* V() */
+/*
+release_semaphore(struct semaphore s) {
+    while(TS(s.mutex)) WAIT(s.m);   // wait for intertal mutex 
+    s.value++;
+    if(s.value >= 0) {
+        s.hold = FALSE;
+        SIGNAL(s.h);
+    }
+    s.mutex = FALSE;
+    SIGNAL(s.m);
+}
+*/
 int compare_and_swap(int *value, int expected, int new_value) {
 	int temp = *value;
 	if (*value == expected)
